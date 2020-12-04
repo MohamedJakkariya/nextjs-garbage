43f91ae68327e3d05c5e848f5286a4d27804a142
MohamedJakkariya <mohamed.jakkariya@fleksa.com>
2020-12-04 21:32:15 +0800

    added delete functions

--- pages/components/note.tsx
+++ pages/components/note.tsx
@@ -1,13 +1,29 @@
 /** @format */
 
-export default function Note() {
+import { useState } from 'react';
+
+export default function Note(props) {
+	const [description, setDescription] = useState('');
+
+	const handleDescriptionChange = (v) => {
+		setDescription(v);
+		props.onDescription(v, props.index);
+	};
+
 	return (
 		<div className='note-box'>
 			<div className='header'>
-				<i className='far fa-edit'></i>
-				<i className='far fa-trash-alt'></i>
+				<span className='far fa-edit'></span>
+				<span
+					className='far fa-trash-alt'
+					onClick={() => props.onDelete(props.index)}></span>
 			</div>
-			<textarea className='text-input' name='note' cols={30}></textarea>
+			<textarea
+				className='text-input'
+				onChange={(e) => handleDescriptionChange(e.target.value)}
+				value={description}
+				name='note'
+				cols={30}></textarea>
 		</div>
 	);
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,28 @@
 /** @format */
 import Head from 'next/head';
+import { useEffect, useState } from 'react';
 import Note from './components/note';
 
 export default function Home() {
+	const [notes, setNotes] = useState(['', '']);
+
+	useEffect(() => {
+		console.log(notes);
+	}, [notes]);
+
+	const hanldeDeleteButtonClick = (i) => {
+		const result = notes.filter((item, index) => i !== index);
+		setNotes(result);
+	};
+
+	const hanldeAddButtonClick = () => setNotes((notes) => [...notes, '']);
+
+	const hanldeDescriptionChange = (text, index) => {
+		notes[index] = text;
+		setNotes(notes);
+		console.log(notes);
+	};
+
 	return (
 		<div className='container'>
 			<Head>
@@ -12,28 +32,21 @@ export default function Home() {
 			</Head>
 			<div className='container-header'>
 				<p></p>
-				<button className='add-btn'>
+				<button onClick={hanldeAddButtonClick} className='add-btn'>
 					<i className='fas fa-plus'></i> Add Note
 				</button>
 			</div>
 
 			<div className='notes-container'>
-				<Note />
-				<Note />
-				<Note />
-				<Note />
-				<Note />
-				<Note />
-				<Note />
-				<Note />
-				<Note />
-				<Note />
-				<Note />
-				<Note />
-				<Note />
-				<Note />
-				<Note />
-
+				{notes.map((note, index) => (
+					<Note
+						key={index}
+						description={note}
+						onDelete={hanldeDeleteButtonClick}
+						index={index}
+						onDescription={hanldeDescriptionChange}
+					/>
+				))}
 			</div>
 		</div>
 	);
